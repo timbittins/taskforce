a87553db9227b242df7f6fc3c34a107ef4be79a9
Tim Bittins <tech@timbittins.com>
2021-10-11 12:47:18 +0200

    initial commit

---
+++ apps/app.js
@@ -0,0 +1,26 @@
+"use strict";
+const log = console.log.bind(console);
+function getSelector(getElement) {
+  const element = document.querySelector(getElement);
+  if (element) {
+    return element;
+  } else {
+    throw Error(`There is no element called ${getElement}`);
+  }
+}
+function getSelectors(getElements) {
+  const elements = document.querySelectorAll(getElements);
+  if (elements.length == 0) {
+    throw Error(`There are no elements called ${getElements}`);
+  } else {
+    return elements;
+  }
+}
+function getID(getID) {
+  const element = document.getElementById(getID);
+  if (element) {
+    return element;
+  } else {
+    throw Error(`There is no ID called ${getID}`);
+  }
+}
